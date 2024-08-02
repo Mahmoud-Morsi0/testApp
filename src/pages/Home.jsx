337a2989ccfb8b6182ba6e0f0786faacbd4077a5
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2024-08-02 17:05:41 +0300

    get products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,20 @@ import Card from './../components/Card/index';
 import axios from 'axios';
 export default function Home() {
 
-    const [data, setData] = useState(undefined)
-    console.log(data)
+    const [products, setProducts] = useState([])
+    console.log(products)
 
 
     useEffect(() => {
         const getData = async () => {
             const { data } = await axios.get('https://ecommerce.routemisr.com/api/v1/products')
-            console.log(data)
-            setData(data)
+            setProducts(data)
         }
         getData()
     }, [])
     return (
         <div className="custom-height flex justify-center items-start flex-wrap  w-screen " >
-            <Card data={data.data} />
+            <Card data={products.data} />
 
         </div>
     )
